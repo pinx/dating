6af05d96d89574fe14ac16818bb00f021778863b
Tadeusz Łazurski <tadeusz@lazurski.pl>
2017-12-31 10:47:15 +0100

    Restructure and create more tests
    Tests are organized into groups based on the relation of a and b
    periods.
    
    I renamed period1 to a and period2 to b. I think it is more visually
    distinct and so clear that way.

--- test/dating_test.exs
+++ test/dating_test.exs
@@ -3,95 +3,286 @@ defmodule DatingTest do
   doctest Dating
   alias Dating.{Period}
 
-  describe "intersect" do
-    test "overlapping periods" do
-      period1 = %Period{
+
+  describe "intersect identical periods" do
+
+    test "simple" do
+      a = %Period{
         start_at: ~D[2017-01-09],
-        end_at: ~D[2017-07-14]}
+        end_at: ~D[2017-07-14]
+      }
 
-      period2 = %Period{
-        start_at: ~D[2017-03-03],
-        end_at: ~D[2017-09-08]}
-
-      result = %Period{
-        start_at: ~D[2017-03-03],
-        end_at: ~D[2017-07-14]}
-
-      assert result == Period.intersect(period1, period2)
-    end
-
-    test "non-overlapping periods" do
-      period1 = %Period{
+      b = %Period{
         start_at: ~D[2017-01-09],
-        end_at: ~D[2017-02-14]}
+        end_at: ~D[2017-07-14]
+      }
 
-      period2 = %Period{
-        start_at: ~D[2017-03-03],
-        end_at: ~D[2017-09-08]}
+      result =
+        %Period{
+          start_at: ~D[2017-01-09],
+          end_at: ~D[2017-07-14]
+        }
 
-      Period.intersect(period1, period2)
-      |> IO.inspect()
-      assert is_nil(Period.intersect(period1, period2))
+      assert result == Period.intersect(a, b)
     end
 
-    test "encompassing periods" do
-      period1 = %Period{
+    test "infinite" do
+      a = %Period{
+        start_at: nil,
+        end_at: nil
+      }
+
+      b = %Period{
+        start_at: nil,
+        end_at: nil
+      }
+
+      result =
+        %Period{
+          start_at: nil,
+          end_at: nil
+        }
+
+      assert result == Period.intersect(a, b)
+    end
+
+    test "forever until" do
+      a = %Period{
+        start_at: nil,
+        end_at: ~D[2012-01-01]
+      }
+
+      b = %Period{
+        start_at: nil,
+        end_at: ~D[2012-01-01]
+      }
+
+      result =
+        %Period{
+          start_at: nil,
+          end_at: ~D[2012-01-01]
+        }
+
+      assert result == Period.intersect(a, b)
+    end
+
+    test "forever after" do
+      a = %Period{
+        start_at: ~D[2012-01-01],
+        end_at: nil
+      }
+
+      b = %Period{
+        start_at: ~D[2012-01-01],
+        end_at: nil
+      }
+
+      result =
+        %Period{
+          start_at: ~D[2012-01-01],
+          end_at: nil
+        }
+
+      assert result == Period.intersect(a, b)
+    end
+
+
+  end
+
+  describe "intersect contained periods" do
+
+    test "b inside a" do
+      a = %Period{
         start_at: ~D[2017-01-09],
-        end_at: ~D[2017-09-14]}
+        end_at: ~D[2017-09-14]
+      }
 
-      period2 = %Period{
+      b = %Period{
         start_at: ~D[2017-03-03],
-        end_at: ~D[2017-09-08]}
+        end_at: ~D[2017-09-08]
+      }
 
-      result = period2
-      assert result == Period.intersect(period1, period2)
+      result = b
+
+      assert result == Period.intersect(a, b)
     end
 
-    test "infinite periods" do
-      period1 = %Period{
-        start_at: nil,
-        end_at: ~D[2017-09-14]}
-
-      period2 = %Period{
+    test "a inside b" do
+      a = %Period{
         start_at: ~D[2017-03-03],
-        end_at: ~D[2017-09-16]}
+        end_at: ~D[2017-09-08]
+      }
+
+      b = %Period{
+        start_at: ~D[2017-01-09],
+        end_at: ~D[2017-09-14]
+      }
+
+      result = a
+
+      assert result == Period.intersect(a, b)
+    end
+
+    test "b inside a forever until" do
+      a = %Period{
+        start_at: nil,
+        end_at: ~D[2017-09-14]
+      }
+
+      b = %Period{
+        start_at: ~D[2017-03-03],
+        end_at: ~D[2017-09-08]
+      }
+
+      result = b
+
+      assert result == Period.intersect(a, b)
+    end
+
+    test "b inside a forever after" do
+      a = %Period{
+        start_at: ~D[2017-01-09],
+        end_at: nil
+      }
+
+      b = %Period{
+        start_at: ~D[2017-03-03],
+        end_at: ~D[2017-09-08]
+      }
+
+      result = b
+
+      assert result == Period.intersect(a, b)
+    end
+
+    test "b is forever, a is forever until" do
+      a = %Period{
+        start_at: nil,
+        end_at: ~D[2016-02-04]
+      }
+
+      b = %Period{
+        start_at: nil,
+        end_at: nil
+      }
+
+      result = a
+
+      assert result == Period.intersect(a, b)
+    end
+
+  end
+
+  describe "intersect partially overlapping periods" do
+
+    test "b starts and ends after a" do
+      a = %Period{
+        start_at: ~D[2017-01-09],
+        end_at: ~D[2017-07-14]
+      }
+
+      b = %Period{
+        start_at: ~D[2017-03-03],
+        end_at: ~D[2017-09-08]
+      }
 
       result = %Period{
         start_at: ~D[2017-03-03],
-        end_at: ~D[2017-09-14]}
+        end_at: ~D[2017-07-14]
+      }
 
-      assert result == Period.intersect(period1, period2)
+      assert result == Period.intersect(a, b)
     end
 
-    test "two infinite periods" do
-      period1 = %Period{
-        start_at: nil,
-        end_at: nil}
+    test "b starts and ends after a, one day overlapping" do
+      a = %Period{
+        start_at: ~D[2017-01-09],
+        end_at: ~D[2017-02-14]
+      }
 
-      period2 = %Period{
-        start_at: nil,
-        end_at: nil}
+      b = %Period{
+        start_at: ~D[2017-02-14],
+        end_at: ~D[2017-09-08]
+      }
 
       result = %Period{
-        start_at: nil,
-        end_at: nil}
+        start_at: ~D[2017-02-14],
+        end_at: ~D[2017-02-14]
+      }
 
-      assert result == Period.intersect(period1, period2)
+      assert result == Period.intersect(a, b)
     end
-    test "infinite with semi infinite period" do
-      period1 = %Period{
-        start_at: nil,
-        end_at: ~D[2016-02-04]}
 
-      period2 = %Period{
+    test "b overlaps with a forever until" do
+      a = %Period{
         start_at: nil,
-        end_at: nil}
+        end_at: ~D[2017-09-14]
+      }
+
+      b = %Period{
+        start_at: ~D[2017-03-03],
+        end_at: ~D[2017-09-16]
+      }
 
       result = %Period{
-        start_at: nil,
-        end_at: ~D[2016-02-04]}
+        start_at: ~D[2017-03-03],
+        end_at: ~D[2017-09-14]
+      }
 
-      assert result == Period.intersect(period1, period2)
+      assert result == Period.intersect(a, b)
     end
+
+    test "b is forever after, a is forever until" do
+      a = %Period{
+        start_at: nil,
+        end_at: ~D[2017-09-14]
+      }
+
+      b = %Period{
+        start_at: ~D[2017-03-03],
+        end_at: nil
+      }
+
+      result = %Period{
+        start_at: ~D[2017-03-03],
+        end_at: ~D[2017-09-14]
+      }
+
+      assert result == Period.intersect(a, b)
+    end
+
+  end
+
+  describe "non-overlapping periods" do
+
+    test "b after a, non-neighbouring" do
+      a = %Period{
+        start_at: ~D[2017-01-09],
+        end_at: ~D[2017-02-14]
+      }
+
+      b = %Period{
+        start_at: ~D[2017-03-03],
+        end_at: ~D[2017-09-08]
+      }
+
+      Period.intersect(a, b)
+      assert is_nil(Period.intersect(a, b))
+    end
+
+    test "b after a, neighbouring" do
+      a = %Period{
+        start_at: ~D[2017-01-09],
+        end_at: ~D[2017-02-14]
+      }
+
+      b = %Period{
+        start_at: ~D[2017-02-15],
+        end_at: ~D[2017-09-08]
+      }
+
+      assert is_nil(Period.intersect(a, b))
+    end
+
   end
 end
