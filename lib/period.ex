418e121303a83678dff92aaf7f02bf5129a6b95f
Marcel van Pinxteren <marcel.van.pinxteren@gmail.com>
2018-02-22 10:20:55 +0100

    Remove Apex dependency

--- lib/period.ex
+++ lib/period.ex
@@ -1,19 +1,21 @@
 defmodule Periods.Period do
   defstruct starts_at: nil, ends_at: nil
 
-  defimpl Inspect do
-    import Inspect.Algebra
+# For debugging purposes, you could uncomment the section below.
+#
+#   defimpl Inspect do
+#     import Inspect.Algebra
 
-    def inspect(period, opts) do
-      concat [to_string(period.starts_at), " — ", to_string(period.ends_at)]
-    end
-  end
+#     def inspect(period, opts) do
+#       concat [to_string(period.starts_at), " — ", to_string(period.ends_at)]
+#     end
+#   end
 
-  defimpl Apex.Format do
-    import Apex.Format.Utils
+#   defimpl Apex.Format do
+#     import Apex.Format.Utils
 
-    def format(data, options \\ []) do
-      colorize(inspect(data), 0, options) <> new_line()
-    end
-  end
+#     def format(data, options \\ []) do
+#       colorize(inspect(data), 0, options) <> new_line()
+#     end
+#   end
 end
